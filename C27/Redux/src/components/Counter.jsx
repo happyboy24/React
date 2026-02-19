4595c1f15c7bd12d75fc0ad0d313804647cbc8d1
happyboy24 <cejika134@gmail.com>
2026-02-19 15:35:03 +0100

    crazy commit

--- C27/Redux/src/components/Counter.jsx
+++ C27/Redux/src/components/Counter.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import App from '../App'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement,reset } from '../slices/counterSlices';
 
 const Counter = () => {
+    const value = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+    
   return (
     <div>
-      <span>0</span>
-        <button>+</button>
-        <button>-</button>
+      <span>{value}</span>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   )
 }
 
 
-export default Counter
+export default Counter;
